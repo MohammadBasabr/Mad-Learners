183051f5fdbe4333160298fafe15a32ba2242d32
MonirehBastami <monirehbastami@gmail.com>
2023-02-20 12:39:15 -0800

    add courses reducer

---
+++ src/context/course/course.reducer.ts
@@ -0,0 +1,11 @@
+import { ContextAction, ContextActionTypes, CoursesListState } from "@/@types/context/context.type";
+
+export const CourseReducer = (state:CoursesListState,action:ContextAction<ContextActionTypes,any>)=>{
+    switch (action.type){
+        case ContextActionTypes.Get_All_Courses:
+            state.coursesList = action.payload
+        return state;
+        default:
+            return state;
+    }
+}
