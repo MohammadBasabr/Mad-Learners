6421cb4c9263ce2c30e4403451337ada37caaa5b
MonirehBastami <monirehbastami@gmail.com>
2023-02-20 17:29:33 -0800

    add delete current course

--- src/@types/context/context.type.ts
+++ src/@types/context/context.type.ts
@@ -2,26 +2,30 @@ export type CreateContext = {
     state:ContextAppState,
     dispatch:React.Dispatch<ContextAction<any,any>>
 }
+export enum ContextActionTypes{
+    Get_All_Courses = "Get_All_Courses",
+    Delete_Current_course = "Delete_Current_course"
+}
+export type ContextAction<T, K> = {
+    type: T;
+    payload?: K;
+};
 export type ContextAppState = {
     courses: CoursesListState;
 };
 export type CoursesListState = {
     coursesList: CoursesState[];
 };
-export enum ContextActionTypes{
-    Get_All_Courses = "Get_All_Courses"
-}
-export type ContextAction<T, K> = {
-    type: T;
-    payload?: K;
-};
+
+
 export type CoursesState = {
-    title: string;
-    cost: number;
-    endDate: string;
-    startDate: string;
-    capacity: number;
-    teacher:string;
-    lesson:string;
+    topics: string[];
+    _id: string;
+    lessonName: string;
+    image: string;
+    description: string;
+    category:number;
+    __v:number
 };
+
   
